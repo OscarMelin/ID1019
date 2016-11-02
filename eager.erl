0a256ccf7f39b1c574590476b25fc0d758ef991a
Oscar Melin <oscarme@kth.se>
2016-11-02 19:08:41 +0100

    add skeleton code for eager.erl

--- eager.erl
+++ eager.erl
@@ -1,2 +1,24 @@
 -module(eager).
 -export([eval_expr/2]).
+
+eval_expr({atm, Id}, _) ->
+	{ok, Id};
+eval_expr({var, Id}, Env) ->
+	case expression of
+		false ->
+			error;
+		... ->
+			{ok, ..}
+	end;
+eval_expr({cons, ..., ...} Env) ->
+	case ... of
+		error ->
+			...;
+		{ok, ...} ->
+			case ... of
+				error ->
+					error;
+				{ok, ...} ->
+					{ok, {..., ...}}
+			end
+	end.
