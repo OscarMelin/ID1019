b5b1b8bec77724e81ad9dc7afd5b8553970bcab3
Oscar Melin <oscarme@kth.se>
2016-11-25 17:59:21 +0100

    start dining filisophers

---
+++ Dining filisophers/chopstick.erl
@@ -0,0 +1,18 @@
+start() ->
+    spawn_link(fun() -> ... end).
+
+availiable() ->
+    recieve
+        ... ->
+            :
+        quit ->
+            ok
+    end.
+
+gone() ->
+    recieve
+        ... ->
+            :
+        quit ->
+            ok
+    end.
