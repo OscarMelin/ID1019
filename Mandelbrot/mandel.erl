f8b2558f53c5c1a6e340809160840e3a266288cc
Oscar Melin <oscarme@kth.se>
2017-02-08 17:39:00 +0100

    mandel done?

--- Mandelbrot/mandel.erl
+++ Mandelbrot/mandel.erl
@@ -7,4 +7,31 @@ mandelbrot(Width, Height, X, Y, K, Depth) ->
 		end,
 	rows(Width, Height, Trans, Depth, []).
 
-rows()
+
+rows(_, 0, _, _, Image)->
+  	Image;
+rows(Width, Height, Trans, Depth, Image) ->
+	Line = line(Width, Height, Trans, Depth, []),
+	rows(Width, Height - 1, Trans, Depth, [Line | Image]).
+
+line(0, _, _, _, Line)->
+	Line;
+line(Width, Height, Trans, Depth, Line) ->
+	PixelDepth = brot:mandelbrot(Trans(Width, Height), Depth),
+	PixelColor = color:convert(PixelDepth, Depth),
+	line(Width - 1, Height, Trans, Depth, Line).
+	
+
+demo() ->
+	small(-2.6,1.2,1.6).
+
+small(X,Y,X1) ->
+	Width = 960,
+	Height = 540,
+	K = (X1 - X)/Width,
+	Depth = 64,
+	T0 = now(),
+	Image = mandelbrot(Width, Height, X, Y, K, Depth),
+	T = timer:now_diff(now(), T0),
+	io:format("picture generated in ~w ms~n", [T div 1000]),
+	ppm:write("small.ppm", Image).
